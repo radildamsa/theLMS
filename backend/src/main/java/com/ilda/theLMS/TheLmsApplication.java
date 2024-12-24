71f57ea2805b3a564edbcf051128a7b45b8d1ba8
OZT00150 <radilr@cbctechsol.com>
2024-12-24 11:41:34 +0530

    Initial commit theLMS

---
+++ backend/src/main/java/com/ilda/theLMS/TheLmsApplication.java
@@ -0,0 +1,13 @@
+package com.ilda.theLMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLmsApplication.class, args);
+	}
+
+}
